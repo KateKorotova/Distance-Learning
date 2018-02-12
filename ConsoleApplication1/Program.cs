4d4969fd0f0c88c379c3a70e8f59d2ed71b719b6
Kate <Kate@DESKTOP-O7T77KT>
2018-02-12 21:51:30 +0200

    Task 3: create struct Star, accept quantity of stars

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,99 +8,139 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int cx = 20;
+        static int cy = 10;
+
+        static int numStars()
         {
-            int cx = 20;
-            int cy = 10;
-            int stepx1 = 1;
-            int stepy1 = 1;
-            int stepx2 = 1;
-            int stepy2 = 1;
-            Random r = new Random();
-            int x1 = r.Next(0, cx);
-            int y1 = r.Next(0, cy);
-            int x2 = r.Next(0, cx);
-            int y2 = r.Next(0, cy);
+            Console.WriteLine("How many stars do you prefer?");
+            int num;
+            bool ques = Int32.TryParse(Console.ReadLine(), out num);
+            if (ques == false)
+                throw new ArgumentException("Non valid data");
+            return num;
+        }
 
-            for (;;)
+       public struct Star
+        {
+            public Star(int x, int y)
             {
-                Console.CursorVisible = false;
-
-
-                if (x1 >= cx)
-                {
-                    stepx1 *= -1;
-                    x1 = cx;
-                }
-                if (y1 >= cy)
-                {
-                    stepy1 *= -1;
-                    y1 = cy;
-                }
-                if (x1 <= 0)
-                {
-                    stepx1 *= -1;
-                    x1 = 0;
-                }
-                if (y1 <= 0)
-                {
-                    stepy1 *= -1;
-                    y1 = 0;
-                }
-
-
-                if (x2 >= cx)
-                {
-                    stepx2 *= -1;
-                    x2 = cx;
-                }
-                if (y2 >= cy)
-                {
-                    stepy2 *= -1;
-                    y2 = cy;
-                }
-                if (x2 <= 0)
-                {
-                    stepx2 *= -1;
-                    x2 = 0;
-                }
-                if (y2 <= 0)
-                {
-                    stepy2 *= -1;
-                    y2 = 0;
-                }
-
-
-
-                if ((x1 == x2 + 1 || x1 + 1 == x2 || x1 == x2) && y1 == y2)
-                {
-                    x1 = x2; 
-                    stepx1 *= -1;
-                    stepx2 *= -1;
-                    stepy1 *= -1;
-                }
-                if ((y1 == y2 + 1 || y2 == y1 + 1 || y1 == y2) && x1 == x2)
-                {
-                    y1 = y2;
-                    stepy1 *= -1;
-                    stepy2 *= -1;
-                    stepx2 *= -1; 
-                }
-
-                Console.SetCursorPosition(x2, y2);
-                Console.Write('*');
-                Console.SetCursorPosition(x1, y1);
-                Console.Write('*');
-
-                x1 += stepx1;
-                y1 += stepy1;
-
-                x2 += stepx2;
-                y2 += stepy2;
-
-                System.Threading.Thread.Sleep(60);
-                Console.Clear();
+                this.x = x;
+                this.y = y;
+                this.stepy = 1;
+                this.stepx = 1; 
             }
+            public int x;
+            public int y;
+            public int stepx;
+            public int stepy;
+        }
+
+        static Star[] createArr(int quantity)
+        {
+            Random r = new Random();
+            Star[] stars = new Star[quantity];
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].x = r.Next(0, cx);
+                stars[i].y = r.Next(0,cy);
+            }
+            return stars; 
+        }
+
+        static void paint(Star[] stars)
+        {
+            for (int i = 0; i < stars.Length; i++)
+            {
+
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        { 
+
+            //for (;;)
+            //{
+            //    Console.CursorVisible = false;
+
+
+            //    if (x1 >= cx)
+            //    {
+            //        stepx1 *= -1;
+            //        x1 = cx;
+            //    }
+            //    if (y1 >= cy)
+            //    {
+            //        stepy1 *= -1;
+            //        y1 = cy;
+            //    }
+            //    if (x1 <= 0)
+            //    {
+            //        stepx1 *= -1;
+            //        x1 = 0;
+            //    }
+            //    if (y1 <= 0)
+            //    {
+            //        stepy1 *= -1;
+            //        y1 = 0;
+            //    }
+
+
+            //    if (x2 >= cx)
+            //    {
+            //        stepx2 *= -1;
+            //        x2 = cx;
+            //    }
+            //    if (y2 >= cy)
+            //    {
+            //        stepy2 *= -1;
+            //        y2 = cy;
+            //    }
+            //    if (x2 <= 0)
+            //    {
+            //        stepx2 *= -1;
+            //        x2 = 0;
+            //    }
+            //    if (y2 <= 0)
+            //    {
+            //        stepy2 *= -1;
+            //        y2 = 0;
+            //    }
+
+
+
+            //    if ((x1 == x2 + 1 || x1 + 1 == x2 || x1 == x2) && y1 == y2)
+            //    {
+            //        x1 = x2; 
+            //        stepx1 *= -1;
+            //        stepx2 *= -1;
+            //        stepy1 *= -1;
+            //    }
+            //    if ((y1 == y2 + 1 || y2 == y1 + 1 || y1 == y2) && x1 == x2)
+            //    {
+            //        y1 = y2;
+            //        stepy1 *= -1;
+            //        stepy2 *= -1;
+            //        stepx2 *= -1; 
+            //    }
+
+            //    Console.SetCursorPosition(x2, y2);
+            //    Console.Write('*');
+            //    Console.SetCursorPosition(x1, y1);
+            //    Console.Write('*');
+
+            //    x1 += stepx1;
+            //    y1 += stepy1;
+
+            //    x2 += stepx2;
+            //    y2 += stepy2;
+
+            //    System.Threading.Thread.Sleep(60);
+            //    Console.Clear();
+            //}
         }
     }
 }
