1bf00926af327909981eeedc5d462abe27e1e5ac
Kate <Kate@DESKTOP-O7T77KT>
2018-02-12 17:23:23 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10,
+    b = a / 5;
+
+            if (b <= 2)
+            {
+                Console.WriteLine("Менше або дорівнює двом");
+            }
+            else if (b == 2)
+            {
+                Console.WriteLine("Дорівнює двом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
