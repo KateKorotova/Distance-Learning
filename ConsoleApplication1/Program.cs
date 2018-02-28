762412f7a136819fc4044dd63984b1e4f3a97270
Kate <Kate@DESKTOP-O7T77KT>
2018-02-28 17:13:23 +0200

    Task 4: class Ball

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,14 +6,69 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Ball
     {
         static int cx = 50;
         static int cy = 20;
+        internal Ball()
+        {
+            r = 0; 
+        }
+        internal Ball(int x, int y, int r)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = r; 
+            this.stepy = 1;
+            this.stepx = 1;
+        }
+        internal int x;
+        internal int y;
+        internal int stepx;
+        internal int stepy;
+        internal int r; 
+        internal void checkBoard()
+        {
+                if (x + r >= cx)
+                {
+                    stepx *= -1;
+                    x = cx - r;
+                }
+                if (y + r >= cy)
+                {
+                    stepy *= -1;
+                    y = cy - r;
+                }
+                if (x - r <= 0)
+                {
+                    stepx *= -1;
+                    x = r;
+                }
+                if (y - r <= 0)
+                {
+                    stepy *= -1;
+                    y =r;
+                }
+        }
+        internal void paint()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write('*');
+            x += stepx;
+            y += stepy;
+            checkBoard();
+        }
+
+    }
+
+
+    class Program
+    {
+
 
         static int numStars()
         {
-            Console.WriteLine("How many stars do you prefer?");
+            Console.WriteLine("How many balls do you prefer?");
             int num;
             bool ques = Int32.TryParse(Console.ReadLine(), out num);
             if (ques == false || num == 0)
@@ -21,104 +76,54 @@ namespace ConsoleApplication1
             return num;
         }
 
-        public struct Star
-        {
-            public Star(int x, int y)
-            {
-                this.x = x;
-                this.y = y;
-                this.stepy = 1;
-                this.stepx = 1;
-            }
-            public int x;
-            public int y;
-            public int stepx;
-            public int stepy;
-        }
-
-        static Star[] createArr(int quantity)
+        static Ball[] createArr(int quantity)
         {
             Random r = new Random();
-            Star[] stars = new Star[quantity];
-            for (int i = 0; i < stars.Length; i++)
+            Ball[] balls = new Ball[quantity];
+            for (int i = 0; i < balls.Length; i++)
             {
-                stars[i] = new Star(r.Next(0, cx), r.Next(0, cy));
+                balls[i] = new Ball(r.Next(0, 20), r.Next(0, 50), 0);
 
             }
-            return stars; 
+            return balls; 
         }
 
-        static void checkBoard(ref Star[] stars)
-        {
-            for ( int i = 0; i < stars.Length; i++)
-            {
-                if (stars[i].x >= cx)
-                {
-                    stars[i].stepx *= -1;
-                    stars[i].x = cx;
-                }
-                if (stars[i].y >= cy)
-                {
-                    stars[i].stepy *= -1;
-                    stars[i].y = cy;
-                }
-                if (stars[i].x <= 0)
-                {
-                    stars[i].stepx *= -1;
-                    stars[i].x = 0;
-                }
-                if (stars[i].y <= 0)
-                {
-                    stars[i].stepy *= -1;
-                    stars[i].y = 0;
-                }
-            }
-        }
 
-        static void checkPos(ref Star[] stars)
+        static void checkPos(ref Ball[] balls)
         {
-            for (int i = 0; i < stars.Length; i++)
-                for( int j = i + 1; j < stars.Length; j++)
+            for (int i = 0; i < balls.Length; i++)
+                for( int j = i + 1; j < balls.Length; j++)
                 {
-                    if ((stars[i].x == stars[j].x + 1 || stars[i].x + 1 == stars[j].x || stars[i].x == stars[j].x) && stars[i].y == stars[j].y)
+                    if ((balls[i].x + balls[i].r + 1 == balls[j].x - balls[j].r || balls[i].x - balls[i].r  == balls[j].x + balls[j].r + 1 || balls[i].x + balls[i].r == balls[j].x + balls[j].r) 
+                        && balls[i].y == balls[j].y)
                     {
-                        stars[i].x = stars[j].x;
-                        stars[i].stepx *= -1;
-                        stars[j].stepx *= -1;
-                        stars[i].stepy *= -1;
+                        balls[i].stepx *= -1;
+                        balls[j].stepx *= -1;
+                        balls[i].stepy *= -1;
                     }
-                    if ((stars[i].y == stars[j].y + 1 || stars[i].y == stars[j].y + 1 || stars[i].y == stars[j].y) && stars[i].x == stars[j].x)
+                    if ((balls[i].y - balls[i].r == balls[j].y + balls[j].r + 1 || balls[i].y - balls[i].r == balls[j].y + balls[j].r + 1 || balls[i].y + balls[i].r == balls[j].y + balls[j].r) 
+                        && balls[i].x == balls[j].x)
                     {
-                        stars[i].y = stars[j].y;
-                        stars[i].stepy *= -1;
-                        stars[j].stepy *= -1;
-                        stars[j].stepx *= -1;
+                        balls[i].y = balls[j].y;
+                        balls[i].stepy *= -1;
+                        balls[j].stepy *= -1;
+                        balls[j].stepx *= -1;
                     }
                 }
         }
 
-        static void paint(ref Star[] stars)
+        static void paint(ref Ball[] balls)
         {
             while (true)
             {
                 Console.CursorVisible = false;
-
-                for (int i = 0; i < stars.Length; i++)
-                {
-                    Console.SetCursorPosition(stars[i].x, stars[i].y);
-                    Console.Write('*');
-                    stars[i].x += stars[i].stepx;
-                    stars[i].y += stars[i].stepy;
-                }
-
-                checkBoard(ref stars);
-                checkPos(ref stars);
+                for (int i = 0; i < balls.Length; i++)
+                    balls[i].paint();
+                checkPos(ref balls);
                 System.Threading.Thread.Sleep(50);
                 Console.Clear();
             }
         }
-
-
 
 
         static void Main(string[] args)
@@ -137,8 +142,8 @@ namespace ConsoleApplication1
                     Console.WriteLine(e);
                 }
             }
-            Star[] stars = createArr(quantity);
-            paint(ref stars);
+            Ball[] balls = createArr(quantity);
+            paint(ref balls);
         }
     }
 }
